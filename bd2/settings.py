7b57729594527eeb03e9b6c54829f73bb76d8023
David <jdpulido@unbosque.edu.co>
2025-10-29 02:03:32 -0500

    conexion bd my sql

--- bd2/settings.py
+++ bd2/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'bd2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PROYECTO',
+        'USER': 'jhosman',
+        'PASSWORD': '12345',
+        'HOST': '192.168.2.23',
+        'PORT': '3306',
     }
 }
 
