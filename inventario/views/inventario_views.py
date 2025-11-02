3046c0491e5493d1f4820373c0f3418ec900cae1
David <jdpulido@unbosque.edu.co>
2025-11-02 18:03:37 -0500

    error solucinado login

--- inventario/views/inventario_views.py
+++ inventario/views/inventario_views.py
@@ -1,4 +1,13 @@
+from django.db import connection
 from django.shortcuts import redirect, render
 
+import inventario
+
 def panel_inventario(request):
-    return render(request,'panel_inventario.html')
+
+    with connection.cursor() as cursor:
+        cursor.callproc('sp_inventario_tienda',[1])
+        productos = cursor.fetchall()
+
+    print(productos)    
+    return render(request,'panel_inventario.html',{'productos':productos})
